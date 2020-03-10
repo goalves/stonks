8e930dc1ebed3b57da0cd4b31a9a9ab909b0f320
Gabriel Alves <g04lves@gmail.com>
2020-03-10 08:33:51 -0300

    Start correct applications on test environment

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,14 @@
-ExUnit.start()
+Bureaucrat.start(
+  writer: Bureaucrat.ApiBlueprintWriter,
+  default_path: "docs/documentation.md",
+  paths: [],
+  titles: [],
+  title: "Stonks Bank API",
+  env_var: "API_DOCS",
+  json_library: Jason
+)
+
+ExUnit.start(formatters: [ExUnit.CLIFormatter, Bureaucrat.Formatter])
+Faker.start()
+{:ok, _} = Application.ensure_all_started(:ex_machina)
 Ecto.Adapters.SQL.Sandbox.mode(Stonks.Repo, :manual)
