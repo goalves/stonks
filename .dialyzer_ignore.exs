b294d0967684b528439f4796edc0eda617edc29b
Gabriel Alves <g04lves@gmail.com>
2020-03-10 17:19:52 -0300

    Ignore issue with dialyzer on Router module

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,6 @@
 [
   # Needed because of issue on Phoenix Repo
   {"lib/phoenix/router.ex", :pattern_match_cov, 331},
-  {"lib/phoenix/router.ex", :no_return, 2}
+  {"lib/phoenix/router.ex", :no_return, 2},
+  {"lib/stonks_web/router.ex", :pattern_match, 1}
 ]
